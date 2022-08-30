a5ec3abecb4ac2be445ad1fa99358082a581ae6b
Thiago Carlucci <tcarlucci@gmail.com>
2022-08-30 06:10:30 -0300

    include Error and console

---
+++ console.js
@@ -0,0 +1,11 @@
+// log => registro
+
+let minhaVar = true;
+
+console.log(245);
+console.log("eu sou um texto");
+console.log(minhaVar);
+
+// tratamento de erro!
+
+console.error("Falha catastrófica");
